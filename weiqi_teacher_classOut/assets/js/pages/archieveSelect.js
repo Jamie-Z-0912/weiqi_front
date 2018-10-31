e4d3bc534864cc7600ec7f04e390c9b6ff281bac
Jamie <>
2018-10-31 19:30:24 +0800

    教师绩效查询

--- weiqi_teacher_classOut/assets/js/pages/archieveSelect.js
+++ weiqi_teacher_classOut/assets/js/pages/archieveSelect.js
@@ -13,17 +13,41 @@
         _message = new $Request.dialog({                //提示信息弹框
             animateName : 'messageScale'
         });
-
+    
     //加载事件
     function loadEvent(){
-
+        var select_years = null, select_month = null;
         //返回上一页
         $(document).on('click', 'button#btnHistory', function(){
             $Request.backPage();
         });
         $('#curYears').on('click', function () {
-          $('#yearsSelect').hasClass('active')?$('#yearsSelect').removeClass('active'):$('#yearsSelect').addClass('active');
-				})
+          $('#yearsSelectBox').hasClass('active')?$('#yearsSelectBox').removeClass('active'):$('#yearsSelectBox').addClass('active');
+        });
+			$('#yearsOption').on('click', 'span', function () {
+				$(this).addClass('selected').siblings().removeClass('selected');
+		    select_years = $(this).text();
+		    if(select_years&&select_month){
+					$('#yearsSelectBox').removeClass('active');
+					$('#curYears').text(select_years+'/'+select_month);
+					loadClassInfo(select_years+'-'+select_month);
+					select_years = null;
+					select_month = null;
+					$('#yearsOption span, #monthOption span').removeClass('selected');
+        }
+			});
+			$('#monthOption').on('click', 'span', function () {
+				$(this).addClass('selected').siblings().removeClass('selected');
+				select_month = $(this).text();
+				if(select_years&&select_month){
+					$('#yearsSelectBox').removeClass('active');
+					$('#curYears').text(select_years+'/'+select_month);
+					loadClassInfo(select_years+'-'+select_month);
+					select_years = null;
+					select_month = null;
+					$('#yearsOption span, #monthOption span').removeClass('selected');
+				}
+			})
     }
 
     //加载用户信息
@@ -51,10 +75,10 @@
     }
 
     //加载绩效
-    function loadClassInfo(){
+    function loadClassInfo(date){
         var _userData = _cookie.data();
         //获取绩效信息
-        $Request.getInfo($Request.getRouter('teacherRecord'),{"tid" : _userData['_userId']},function(data){
+        $Request.getInfo($Request.getRouter('teacherRecord'),{"tid" : _userData['_userId'],startTime:date,endTime:date},function(data){
             _loading.hideIt();
             if('undefined' !== typeof data['resultList']){
                 $("#archieveInfo").html(template('tpl-class', data['resultList']));
@@ -63,11 +87,19 @@
     }
 
     $(function(){
+			var thisYears = new Date().getFullYear(),
+				thisMonth = new Date().getMonth()+1;
+			$('#curYears').text(thisYears+'/'+thisMonth);
+			if(thisYears>2018){
+				for (var i=2019; i<=thisYears; i++){
+					$('#yearsOption dd').append('<span>'+i+'</span>');
+				}
+			}
         //判断是否已登录
         if(_cookie.hasInfo()){
             loadUserInfo();             //加载用户信息
             loadEvent();                //加载事件
-            loadClassInfo();            //加载绩效
+            loadClassInfo(thisYears+'-'+thisMonth);            //加载绩效
             _loading.hideIt();          //隐藏加载的Loading框
         }else{
             $Request.linkTo('login');       //返回登录页
